47dffb7307a68db78f900613e3fddff217696342
curtis.chou <curtis.chou@solventosoft.com.tw>
2024-05-30 23:16:26 +0800

    加入專案檔案。

---
+++ GoogleDriveConsole/MyDriveService.cs
@@ -0,0 +1,60 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+
+namespace GoogleDriveConsole
+{
+	public class MyDriveService
+	{
+		private DriveService _driveService;
+
+		public MyDriveService(FileStream credentialJsonFs)
+		{
+			GoogleCredential credential = GoogleCredential.FromStream(credentialJsonFs).CreateScoped(new[] { DriveService.Scope.Drive });
+
+			_driveService = new DriveService(new Google.Apis.Services.BaseClientService.Initializer()
+			{
+				HttpClientInitializer = credential,
+				ApplicationName = "_applicationName"
+			});
+		}
+
+		public IEnumerable<Google.Apis.Drive.v3.Data.File> GetFilesByFolderName(string folderName)
+		{
+			var folders = GetFilesImpl($" trashed = false and mimeType = 'application/vnd.google-apps.folder' and name = '{folderName}' ");
+			foreach (var folder in folders)
+			{
+				var result = GetFilesImpl($" trashed = false and mimeType != 'application/vnd.google-apps.folder' and parents in '{folder.Id}' ");
+				foreach (var file in result)
+					yield return file;
+			}
+		}
+
+		private IEnumerable<Google.Apis.Drive.v3.Data.File> GetFilesImpl(string Q)
+		{
+			FilesResource.ListRequest req = _driveService.Files.List();
+			req.PageSize = 1000;
+			req.Q = Q;
+			req.Fields = "nextPageToken, files(id, name,parents,mimeType,size,capabilities,modifiedTime,webViewLink,webContentLink)";
+			FileList fileFeedList = req.Execute();
+
+			while (fileFeedList != null)
+			{
+				foreach (Google.Apis.Drive.v3.Data.File file in fileFeedList.Files)
+					yield return file;
+
+				if (fileFeedList.NextPageToken == null)
+					break;
+
+				req.PageToken = fileFeedList.NextPageToken;
+				fileFeedList = req.Execute();
+			}
+		}
+
+		public async Task<Google.Apis.Download.IDownloadProgress> DownloadAsync(string Id, FileStream saveFileStream)
+		{
+			var req = _driveService.Files.Get(Id);
+			return await req.DownloadAsync(saveFileStream);
+		}
+	}
+}
---
+++ GoogleDriveConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GoogleDriveConsole
+{
+	internal class Program
+	{
+
+		static async Task Main(string[] args)
+		{
+			using FileStream fs = new FileStream("myJson.json", FileMode.Open, FileAccess.Read);
+			MyDriveService myService = new MyDriveService(fs);
+
+			var files = myService.GetFilesByFolderName("測試共享資料夾").ToList();
+			Console.WriteLine($"files Count = {files.Count}");
+			foreach (var file in files)
+			{
+				Console.WriteLine($"{file.Name} start");
+				using var fStream = new FileStream(file.Name, FileMode.OpenOrCreate);
+				var downloadProcess = await myService.DownloadAsync(file.Id, fStream);
+				Console.WriteLine($"{file.Name} {downloadProcess.Status}");
+			}
+			Console.WriteLine("完成");
+			Console.ReadLine();
+		}
+	}
+}
