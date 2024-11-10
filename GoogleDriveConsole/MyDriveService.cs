5027467c1a8d948d7c949a46b7800e7a67244865
acc2git <acc2gml@gmail.com>
2024-11-10 21:23:04 +0800

    調整縮排

--- GoogleDriveConsole/MyDriveService.cs
+++ GoogleDriveConsole/MyDriveService.cs
@@ -5,69 +5,69 @@ using Google.Apis.Drive.v3.Data;
 
 namespace GoogleDriveConsole
 {
-	public class MyDriveService
-	{
-		private DriveService _driveService;
+    public class MyDriveService
+    {
+        private DriveService _driveService;
 
-		public Action<IDownloadProgress> DownloadProgressChanged;
+        public Action<IDownloadProgress> DownloadProgressChanged;
 
-		public MyDriveService(FileStream credentialJsonFs)
-		{
-			GoogleCredential credential = GoogleCredential.FromStream(credentialJsonFs).CreateScoped(new[] { DriveService.Scope.Drive });
+        public MyDriveService(FileStream credentialJsonFs)
+        {
+            GoogleCredential credential = GoogleCredential.FromStream(credentialJsonFs).CreateScoped(new[] { DriveService.Scope.Drive });
 
-			_driveService = new DriveService(new Google.Apis.Services.BaseClientService.Initializer()
-			{
-				HttpClientInitializer = credential,
-				ApplicationName = "_applicationName"
-			});
-			DownloadProgressChanged = progress => { };
-		}
+            _driveService = new DriveService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "_applicationName"
+            });
+            DownloadProgressChanged = progress => { };
+        }
 
-		public IEnumerable<Google.Apis.Drive.v3.Data.File> GetFilesByFolderName(string folderName)
-		{
-			var folders = GetFilesImpl($" trashed = false and mimeType = 'application/vnd.google-apps.folder' and name = '{folderName}' ");
-			foreach (var folder in folders)
-			{
-				var result = GetFilesImpl($" trashed = false and mimeType != 'application/vnd.google-apps.folder' and parents in '{folder.Id}' ");
-				foreach (var file in result)
-					yield return file;
-			}
-		}
+        public IEnumerable<Google.Apis.Drive.v3.Data.File> GetFilesByFolderName(string folderName)
+        {
+            var folders = GetFilesImpl($" trashed = false and mimeType = 'application/vnd.google-apps.folder' and name = '{folderName}' ");
+            foreach (var folder in folders)
+            {
+                var result = GetFilesImpl($" trashed = false and mimeType != 'application/vnd.google-apps.folder' and parents in '{folder.Id}' ");
+                foreach (var file in result)
+                    yield return file;
+            }
+        }
 
-		private IEnumerable<Google.Apis.Drive.v3.Data.File> GetFilesImpl(string Q)
-		{
-			FilesResource.ListRequest req = _driveService.Files.List();
-			req.PageSize = 1000;
-			req.Q = Q;
-			req.Fields = "nextPageToken, files(id, name,parents,mimeType,size,capabilities,modifiedTime,webViewLink,webContentLink)";
-			FileList fileFeedList = req.Execute();
+        private IEnumerable<Google.Apis.Drive.v3.Data.File> GetFilesImpl(string Q)
+        {
+            FilesResource.ListRequest req = _driveService.Files.List();
+            req.PageSize = 1000;
+            req.Q = Q;
+            req.Fields = "nextPageToken, files(id, name,parents,mimeType,size,capabilities,modifiedTime,webViewLink,webContentLink)";
+            FileList fileFeedList = req.Execute();
 
-			while (fileFeedList != null)
-			{
-				foreach (Google.Apis.Drive.v3.Data.File file in fileFeedList.Files)
-					yield return file;
+            while (fileFeedList != null)
+            {
+                foreach (Google.Apis.Drive.v3.Data.File file in fileFeedList.Files)
+                    yield return file;
 
-				if (fileFeedList.NextPageToken == null)
-					break;
+                if (fileFeedList.NextPageToken == null)
+                    break;
 
-				req.PageToken = fileFeedList.NextPageToken;
-				fileFeedList = req.Execute();
-			}
-		}
+                req.PageToken = fileFeedList.NextPageToken;
+                fileFeedList = req.Execute();
+            }
+        }
 
-		public async Task<Google.Apis.Download.IDownloadProgress> DownloadAsync(string Id, FileStream saveFileStream)
-		{
-			var req = _driveService.Files.Get(Id);
-			req.MediaDownloader.ProgressChanged += DownloadProgressChanged;
-			return await req.DownloadAsync(saveFileStream);
-		}
+        public async Task<Google.Apis.Download.IDownloadProgress> DownloadAsync(string Id, FileStream saveFileStream)
+        {
+            var req = _driveService.Files.Get(Id);
+            req.MediaDownloader.ProgressChanged += DownloadProgressChanged;
+            return await req.DownloadAsync(saveFileStream);
+        }
 
-		public async Task<Google.Apis.Download.IDownloadProgress> DownloadAsync(string Id, FileStream saveFileStream, int chunkSize)
-		{
-			var req = _driveService.Files.Get(Id);
-			req.MediaDownloader.ProgressChanged += DownloadProgressChanged;
-			req.MediaDownloader.ChunkSize = chunkSize;
-			return await req.DownloadAsync(saveFileStream);
-		}
-	}
+        public async Task<Google.Apis.Download.IDownloadProgress> DownloadAsync(string Id, FileStream saveFileStream, int chunkSize)
+        {
+            var req = _driveService.Files.Get(Id);
+            req.MediaDownloader.ProgressChanged += DownloadProgressChanged;
+            req.MediaDownloader.ChunkSize = chunkSize;
+            return await req.DownloadAsync(saveFileStream);
+        }
+    }
 }
--- GoogleDriveConsole/Program.cs
+++ GoogleDriveConsole/Program.cs
@@ -3,61 +3,61 @@ using Microsoft.Extensions.Configuration;
 
 namespace GoogleDriveConsole
 {
-	internal class Program
-	{
-		private const int chunkSize = 5 * 1024 * 1024;
+    internal class Program
+    {
+        private const int chunkSize = 5 * 1024 * 1024;
 
-		static async Task Main(string[] args)
-		{
-			var builder = new ConfigurationBuilder()
-				.SetBasePath(Directory.GetCurrentDirectory())
-				.AddJsonFile("appsettings.json", optional: false);
-			IConfiguration _configuration = builder.Build();
-			AppSettingDto? setting = _configuration.Get<AppSettingDto>();
-			if (setting == null)
-			{
-				Console.WriteLine("setting配置錯誤");
-				Console.ReadLine();
-				return;
-			}
+        static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+            IConfiguration _configuration = builder.Build();
+            AppSettingDto? setting = _configuration.Get<AppSettingDto>();
+            if (setting == null)
+            {
+                Console.WriteLine("setting配置錯誤");
+                Console.ReadLine();
+                return;
+            }
 
-			MyDriveService myService = InitMyDriveService(setting.GoogleCredentialFile);
+            MyDriveService myService = InitMyDriveService(setting.GoogleCredentialFile);
 
-			Directory.CreateDirectory(setting.FileSaveFolder);
-			var files = myService.GetFilesByFolderName(setting.GoogleFolderName).ToList();
-			Console.WriteLine($"files Count = {files.Count}");
-			foreach (var file in files)
-			{
-				Console.WriteLine($"{file.Name} start");
-				string savePath = Path.Combine(setting.FileSaveFolder, file.Name);
-				using var fStream = new FileStream(savePath, FileMode.OpenOrCreate);
-				var downloadProcess = await myService.DownloadAsync(file.Id, fStream, chunkSize);
-				Console.WriteLine($"{file.Name} {downloadProcess.Status}");
-			}
-			Console.WriteLine("完成");
-			Console.ReadLine();
-		}
+            Directory.CreateDirectory(setting.FileSaveFolder);
+            var files = myService.GetFilesByFolderName(setting.GoogleFolderName).ToList();
+            Console.WriteLine($"files Count = {files.Count}");
+            foreach (var file in files)
+            {
+                Console.WriteLine($"{file.Name} start");
+                string savePath = Path.Combine(setting.FileSaveFolder, file.Name);
+                using var fStream = new FileStream(savePath, FileMode.OpenOrCreate);
+                var downloadProcess = await myService.DownloadAsync(file.Id, fStream, chunkSize);
+                Console.WriteLine($"{file.Name} {downloadProcess.Status}");
+            }
+            Console.WriteLine("完成");
+            Console.ReadLine();
+        }
 
-		private static MyDriveService InitMyDriveService(string filePath)
-		{
-			using FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-			MyDriveService myService = new MyDriveService(fs);
-			myService.DownloadProgressChanged = (IDownloadProgress progress) =>
-			{
-				switch (progress.Status)
-				{
-					case DownloadStatus.Downloading:
-						Console.WriteLine($"Downloaded {progress.BytesDownloaded / 1024} KB");
-						break;
-					case DownloadStatus.Completed:
-						Console.WriteLine("Download complete.");
-						break;
-					case DownloadStatus.Failed:
-						Console.WriteLine("Download failed.");
-						break;
-				}
-			};
-			return myService;
-		}
-	}
+        private static MyDriveService InitMyDriveService(string filePath)
+        {
+            using FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            MyDriveService myService = new MyDriveService(fs);
+            myService.DownloadProgressChanged = (IDownloadProgress progress) =>
+            {
+                switch (progress.Status)
+                {
+                    case DownloadStatus.Downloading:
+                        Console.WriteLine($"Downloaded {progress.BytesDownloaded / 1024} KB");
+                        break;
+                    case DownloadStatus.Completed:
+                        Console.WriteLine("Download complete.");
+                        break;
+                    case DownloadStatus.Failed:
+                        Console.WriteLine("Download failed.");
+                        break;
+                }
+            };
+            return myService;
+        }
+    }
 }
